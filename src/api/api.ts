2881c559b9f0f0e77388841017f1a2464f4119c8
Sara Duarte <sara.duarte@estudante.ifms.edu.br>
2025-10-20 12:41:26 -0300

    Arrumando aula 20/10

--- src/api/api.ts
+++ src/api/api.ts
@@ -16,10 +16,13 @@ api.interceptors.request.use((config) =>{
 api.interceptors.response.use(
     (response)=>response,
     (error)=>{
+        if(error?.code==="ERR_NETWORK"){
+            window.location.href=`/error?mensagem=${encodeURIComponent("Ligue o servidor! -> NPM RUN DEV:MONGO")}`
+        }
         const status = error?.response?.status;
         if(status===401&&!(error?.response?.config?.url.endsWith("/login"))){
             localStorage.removeItem("token")
-            window.location.href="/login?message=Token inválido!"
+            window.location.href="/login?mensagem=Token inválido!"
         }
         return Promise.reject(error)
     }
