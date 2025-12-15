f1c0ed382095416a08380999baa0c7c947409cef
Elytus <elitinho2502@hotmail.com>
2025-12-15 15:09:42 -0400

    Some vercel bugfix

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,28 +1,28 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL
 })
 //middleware para adicionar o token na requisição
 
-api.interceptors.request.use((config) =>{
+api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token")
-    if(token)
+    if (token)
         config.headers.Authorization = `Bearer ${token}`
     return config
 })
 
 //Redirecionar para o LOGIN quando o usuário não tiver permissão.
 api.interceptors.response.use(
-    (response)=>response,
-    (error)=>{
-        if(error?.code==="ERR_NETWORK"){
-            window.location.href=`/error?mensagem=${encodeURIComponent("Ligue o servidor! -> NPM RUN DEV:MONGO")}`
+    (response) => response,
+    (error) => {
+        if (error?.code === "ERR_NETWORK") {
+            window.location.href = `/error`
         }
         const status = error?.response?.status;
-        if(status===401&&!(error?.response?.config?.url.endsWith("/login"))){
+        if (status === 401 && !(error?.response?.config?.url.endsWith("/login"))) {
             localStorage.removeItem("token")
-            window.location.href="/login?mensagem=Faça login!"
+            window.location.href = "/login"
         }
         return Promise.reject(error)
     }
